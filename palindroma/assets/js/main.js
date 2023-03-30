f0a92724a86b4fbe67d9c322435d6c52d09d53fa
AlvaroArbaiza <122307217+AlvaroArbaiza@users.noreply.github.com>
2023-03-30 15:46:12 +0200

    Creazione variabile button

---
+++ palindroma/assets/js/main.js
@@ -0,0 +1,16 @@
+/*
+Palidroma:
+    - Chiedere all’utente di inserire una parola
+    - Creare una funzione per capire se la parola inserita è palindroma
+
+*/
+
+// Costante che si riconduce all'input id="btn"
+const button = document.getElementById("btn");
+
+let reversed;
+
+
+
+
+
