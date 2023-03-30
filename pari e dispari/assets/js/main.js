e87d16cea4ae7d5a4ad46be4375258e687c46e99
AlvaroArbaiza <122307217+AlvaroArbaiza@users.noreply.github.com>
2023-03-30 16:25:56 +0200

    Creazione cartelle per l'esercizio Pari e Dispari e Struttura base di file e ragionamento su file JS

---
+++ pari e dispari/assets/js/main.js
@@ -0,0 +1,17 @@
+/*
+Pari e Dispari:
+    - L’utente sceglie pari o dispari e inserisce un numero da 1 a 5.
+    - Generiamo un numero random (sempre da 1 a 5) per il computer (usando una funzione).
+    - Sommiamo i due numeri
+    - Stabiliamo se la somma dei due numeri è pari o dispari (usando una funzione)
+    - Dichiariamo chi ha vinto.
+*/
+
+// Creo un input sul DOM per prendere il value ogni volta che verrà inserito
+
+// Creo una funzione con math.random per generare un numero casuale da 1 a 5
+
+// Creo una condizione attraverso la quale verranno sommati i due numeri(input e random)
+
+// In base al risultato delle condizioni( un numero pari o dispari) dichiarerò il vincitore
+
